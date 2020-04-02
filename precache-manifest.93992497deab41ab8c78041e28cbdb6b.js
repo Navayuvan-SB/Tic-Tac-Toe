184d4d769fe8d02e66cd97c84ce98361506a7d5b
navayuvan-sb <ns.navayuvan@gmail.com>
2020-04-02 15:43:37 +0530

    Updates

---
+++ precache-manifest.93992497deab41ab8c78041e28cbdb6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27538e983931d6436183b2ed30a6c7c6",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "3cdebfc82184882c6962",
+    "url": "/Tic-Tac-Toe/static/css/main.a9827d45.chunk.css"
+  },
+  {
+    "revision": "77d181ab02e6a634f0d4",
+    "url": "/Tic-Tac-Toe/static/js/2.3753c32d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe/static/js/2.3753c32d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cdebfc82184882c6962",
+    "url": "/Tic-Tac-Toe/static/js/main.0e6521db.chunk.js"
+  },
+  {
+    "revision": "0529e855b5e18e30e2f9",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.0c1f8c58.js"
+  }
+]);
